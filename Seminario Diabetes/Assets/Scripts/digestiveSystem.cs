1e6f0c7ffa71720958affbf5c480f63314307162
Mariano <43249346+InfernuzIGT@users.noreply.github.com>
2018-11-09 23:34:34 -0300

    Sistema Digestivo

---
+++ Seminario Diabetes/Assets/Scripts/digestiveSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class digestiveSystem : MonoBehaviour {
+
+    public float rotSpeed; //Velocidad de rotacion para el modelo
+    Rigidbody rBody;
+
+	void Awake () {
+        rBody = GetComponent<Rigidbody> ();
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnMouseDrag () {
+        //float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        //transform.Rotate (Vector3.up, -rotX);
+
+        float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        rBody.AddTorque (transform.up * -rotX, ForceMode.VelocityChange);
+
+    }
+
+
+
+}
