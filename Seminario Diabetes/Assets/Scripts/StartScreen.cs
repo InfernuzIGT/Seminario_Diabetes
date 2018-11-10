0b194657acae29099e905c84f63293a9a7287645
Mariano <43249346+InfernuzIGT@users.noreply.github.com>
2018-11-10 03:58:10 -0300

    A plenoo 2

---
+++ Seminario Diabetes/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class StartScreen : MonoBehaviour {
+
+    public GameObject _GAME; //Pantalla del juego
+    Animator anim;
+
+    void Start () {
+        anim = GetComponent<Animator> ();
+        _GAME.SetActive (false);
+    }
+
+    public void startGame () {
+        _GAME.SetActive (true);
+        anim.SetTrigger ("start");
+    }
+
+    public void off () {
+        gameObject.SetActive (false);
+	}
+	
+}
