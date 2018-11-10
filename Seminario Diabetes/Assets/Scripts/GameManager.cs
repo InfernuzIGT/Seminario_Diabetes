1e46a756aa3ced92e207ffe63b2fd33122cd4521
Mariano <43249346+InfernuzIGT@users.noreply.github.com>
2018-11-10 16:24:07 -0300

    Dia 10

--- Seminario Diabetes/Assets/Scripts/GameManager.cs
+++ Seminario Diabetes/Assets/Scripts/GameManager.cs
@@ -19,24 +19,109 @@ public class GameManager : MonoBehaviour {
     public Animator animSide; //Animator de la barra lateral
     public Animator animBackground; //Animator del fondo oscuro
 
+    [Header ("Camara")]
+    public GameObject _camera; //Camara
+    public Transform[] cameraViews; //Posiciones de la camara
+    [Range (0f, 10f)] public float transitionSpeed; //Velocidad de la transicion
+    Transform cameraCurrentView;
+    bool enableTransition, isMoving;
+
+    [Header ("Modelo")]
+    public Transform _model;
+    public BoxCollider boxCollider;
+    [Range (0f, 10f)] public float rotationSpeed; //Velocidad a la que rota el modelo para volver a su posicion inicial
+    [Range (0f, 1f)] public float timeTurnOffRotation; //Tiempo que tarda en apagar la rotacion del modelo
+    Quaternion modelRotY;
+    bool isRotating;
+
 
     void Awake () {
-
+        cameraCurrentView = _camera.transform;
+        modelRotY = _model.localRotation;
 	}
 	
 	void Start () {
         txtGlucose.text = ((int)varGlucose).ToString ();
         txtInsuline.text = ((int)varInsuline).ToString ();
         txtSugar.text = ((int)varSugar).ToString ();
+        cameraCurrentView = cameraViews[0]; //Posicion default
+        enableTransition = true;
+        isMoving = true;
+        StartCoroutine (cameraMovement ()); //Da comienzo a la corrutina que mueve la camara
     }
 
     //Activa/desactiva la barra lateral
     public void sideScreen (bool isActive) {
         animSide.SetBool ("isActive", isActive);
         animBackground.SetBool ("isActive", isActive);
+        boxCollider.enabled = !isActive;
     }
-	
-	
+
+
+    void Update () {
+
+        if (Input.GetKeyDown (KeyCode.Alpha1) && enableTransition) {
+            cameraMove (0, true, false);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha2) && enableTransition) {
+            cameraMove (1, false, true);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha3) && enableTransition) {
+            cameraMove (2, false, true);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha4) && enableTransition) {
+            cameraMove (3, false, true);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha5) && enableTransition) {
+            cameraMove (4, false, true);
+        }
+
+    }
+
+    
+
+    //Mueve la camara de posicion
+    IEnumerator cameraMovement () {
+        while (isMoving) {
+            _camera.transform.position = Vector3.Lerp (_camera.transform.position, cameraCurrentView.localPosition, Time.deltaTime * transitionSpeed);
+
+            Vector3 currentAngle = new Vector3 (
+             Mathf.LerpAngle (_camera.transform.rotation.eulerAngles.x, cameraCurrentView.transform.localRotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+             Mathf.LerpAngle (_camera.transform.rotation.eulerAngles.y, cameraCurrentView.transform.localRotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+             Mathf.LerpAngle (_camera.transform.rotation.eulerAngles.z, cameraCurrentView.transform.localRotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+            _camera.transform.eulerAngles = currentAngle;
+
+            if (isRotating) {
+                Vector3 currentAngleModel = new Vector3 (0, Mathf.LerpAngle (_model.transform.rotation.eulerAngles.y, modelRotY.eulerAngles.y, Time.deltaTime * rotationSpeed),0);
+                _model.transform.eulerAngles = currentAngleModel;
+            }
+
+            yield return new WaitForSeconds (0.01f);
+        }
+        StopCoroutine (cameraMovement ());
+    }
+
+    //Mueve la camara a una posicion, la bloquea para que no se pueda mover mas hasta que finalize el movimiento, 
+    //puede deshabilitar la rotacion del modelo por parte del usuario, y si esta desacomodada la rotacion, la setea a como estaba al comienzo de la escena
+    void cameraMove (int _pos, bool _isBoxEnable, bool _isRotating) {
+        enableTransition = false;
+        cameraCurrentView = cameraViews[_pos]; //Posicion a la que ira la camara
+        boxCollider.enabled = _isBoxEnable; //Si habilita o deshabilita el Box Collider para que se mueva el modelo
+        isRotating = _isRotating; //Si rota el modelo para que se acomode a la posicion inicial para que la camaras funcionen correctamente
+        Invoke ("modelTurnOffRotation", timeTurnOffRotation);
+    }
+
+    //Apaga el reseteo de la rotacion del modelo para volver a su posicion inicial
+    void modelTurnOffRotation () {
+        isRotating = false;
+        enableTransition = true;
+    }
+
 
 
     //END
