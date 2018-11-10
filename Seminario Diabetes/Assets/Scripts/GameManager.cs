ea095f096c8e50aa8f26b364ff41d7e804fc3284
Mariano <43249346+InfernuzIGT@users.noreply.github.com>
2018-11-10 00:30:20 -0300

    Barra inferior

---
+++ Seminario Diabetes/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [Header ("Variables")]
+    [Range (0f, 100f)] public float varGlucose;
+    [Range (0f, 100f)] public float varInsuline;
+    [Range (0f, 100f)] public float varSugar;
+
+    [Header ("Barra Informacion")]
+    public Text txtGlucose;
+    public Text txtInsuline;
+    public Text txtSugar;
+
+
+
+    void Awake () {
+		
+	}
+	
+	void Start () {
+        txtGlucose.text = ((int)varGlucose).ToString ();
+        txtInsuline.text = ((int)varInsuline).ToString ();
+        txtSugar.text = ((int)varSugar).ToString ();
+    }
+	
+	
+
+
+    //END
+}
--- Seminario Diabetes/Assets/Scripts/digestiveSystem.cs
+++ Seminario Diabetes/Assets/Scripts/digestiveSystem.cs
@@ -4,25 +4,22 @@ using UnityEngine;
 
 public class digestiveSystem : MonoBehaviour {
 
-    public float rotSpeed; //Velocidad de rotacion para el modelo
-    Rigidbody rBody;
+    float rotSpeed; //Velocidad de rotacion para el modelo
+    Rigidbody rBody; //Rigidbody utilizado para rotar el modelo
+
+
 
 	void Awake () {
         rBody = GetComponent<Rigidbody> ();
-	}
+        rotSpeed = 100;
+    }
 	
 	void Start () {
 		
 	}
 	
-	void Update () {
-		
-	}
-
+    //Al hacer drag, rota el modelo con friccion
     private void OnMouseDrag () {
-        //float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-        //transform.Rotate (Vector3.up, -rotX);
-
         float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
         rBody.AddTorque (transform.up * -rotX, ForceMode.VelocityChange);
 
